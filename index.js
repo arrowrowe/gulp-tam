74cc6d3643b8b7b69170f9157fe34e6bc553982d
arrowrowe <arrowrowe@gmail.com>
2015-11-18 23:18:15 +0800

    Reformat code (change to es6, extract watcher)

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 var fs = require('fs-extra');
 
 var GT = {
-  load: function (assetsPath) {
-    return {
-      assetsPath: assetsPath,
-      assets: GT.getAssets(assetsPath),
-      clean: GT.tasks.clean,
-      build: GT.tasks.build,
-      watch: GT.tasks.watch
-    };
-  },
-  getAssets: function (assetsPath) {
-    return GT.tam.fillAssets(GT.tam.read(assetsPath), {});
-  },
+  load: assetsPath => ({
+    GT: GT,
+    assetsPath: assetsPath,
+    assets: GT.getAssets(assetsPath),
+    clean: GT.tasks.clean,
+    build: GT.tasks.build,
+    watch: GT.tasks.watch
+  }),
+  getAssets: assetsPath => GT.tam.fillAssets(GT.tam.read(assetsPath), {}),
   tasks: {
     clean: function () {
       var dist = this.assets.dist;
@@ -24,71 +21,11 @@ var GT = {
         assets: this.assetsPath
       });
     },
-    watch: function () {
-      var that = this;
-      var assetsPathReal = fs.realpathSync(this.assetsPath);
-
-      var report;
-      var linked;
-      rebuildAll();
-
-      GT.gulp.watch([
-        this.assetsPath,
-        this.assets.src + '/**/*'
-      ], function (event) {
-        GT.tam.log.trace('File [%s] was "%s".', event.path, event.type);
-        if (event.type === 'deleted') {
-          return;
-        }
-        if (assetsPathReal === event.path) {
-          that.assets = GT.getAssets(that.assetsPath);
-          rebuildAll();
-        } else {
-          rebuildOne(event.path);
-        }
-      });
-
-      function rebuildAll() {
-        report = GT.tam.build(GT.tam.prepare(that.assets));
-        linked = GT.tam.link(report, that.assets.www);
-        fs.writeFileSync(that.assets.linked, JSON.stringify(linked));
-      }
-
-      function rebuildOne(file) {
-
-        GT.tam.log.info('File [%s] needs rebuilding.', file);
-        if (forInCommands(function (command, pkgName) {
-          var pkg = that.assets.packages[pkgName];
-          pkg.name = pkgName;
-          GT.tam.namer(that.assets, pkg)(command);
-          GT.tam.worker.perform(command);
-        }) === 0) {
-          GT.tam.log.info('No commands related found.');
-          return;
-        }
-        linked = GT.tam.link(report, that.assets.www);
-        fs.writeFileSync(that.assets.linked, JSON.stringify(linked));
-
-        function forInCommands(fn) {
-          var i = 0;
-          for (var pkgName in report) {
-            report[pkgName].commands.forEach(function (command) {
-              if (command.files.indexOf(file) >= 0) {
-                i++;
-                fn(command, pkgName);
-              }
-            });
-          }
-          return i;
-        }
-      }
-
-    }
+    watch: require('./lib/watcher')
   }
 };
 
-module.exports = function (gulp, tam) {
-  GT.gulp = gulp;
-  GT.tam = tam;
-  return GT.load;
-};
+module.exports = (gulp, tam) =>
+  (GT.gulp = gulp) &&
+  (GT.tam = tam) &&
+  GT.load;
